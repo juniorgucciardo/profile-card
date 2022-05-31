932dacc989345d4da53d21ce3e86bae72d01514e
junior gucciardo <juniorgucciardo1@gmail.com>
2022-05-31 16:51:46 -0300

    main

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-alert("hello world");
+var menuHeader = document.getElementsByClassName("header-menu");
+var contentHeader = document.getElementsByClassName("main-header");
 
 function openMenu(){
-        alert("teste");
+        if(contentHeader[0].style.display != 'none'){
+            contentHeader[0].style.display = 'none';
+            menuHeader[0].style.display = 'flex'
+        } else {
+            contentHeader[0].style.display = 'flex';
+            menuHeader[0].style.display = 'none';
+        }
+        
     }
